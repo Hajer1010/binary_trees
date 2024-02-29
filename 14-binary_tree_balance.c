0d2ab822686ada99e9175d134ea706067f6b6514
Hajer1010 <hajeer_00@hotmail.com>
2024-02-29 14:59:05 -0800

    done

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,13 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_high - function
+ * @tree: ptr
+ * Return: height
  */
 size_t binary_tree_high(const binary_tree_t *tree)
 {
 	size_t right_h = 0, left_h = 0;
-	
+
 	if (!tree)
 		return (0);
 	left_h = binary_tree_high(tree->left);
@@ -13,11 +16,14 @@ size_t binary_tree_high(const binary_tree_t *tree)
 	return (1 + (left_h > right_h ? left_h : right_h));
 }
 /**
- *
+ * binary_tree_balance - function
+ * @tree: ptr
+ * Return: balance
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int right_h = 0, left_h = 0;
+
 	if (!tree)
 		return (0);
 	if (tree->left)
