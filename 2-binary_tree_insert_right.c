ba0b1a0e213336811e4dd1eff990c4634e5c8e66
Hajer1010 <hajeer_00@hotmail.com>
2024-02-29 12:07:11 -0800

    done

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * *binary_tree_insert_right - function
+ * @parent: ptr
+ * @value: value
+ * Return: ptr
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	if (!parent)
+		return NULL;
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = parent;
+	if (parent->right)
+	{
+		parent->right->parent = node;
+		node->right = parent->right;
+	}
+	parent->right = node;
+
+	return (node);
+}
